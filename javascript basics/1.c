b292c12bf4773cdb73b52b14c0c2aad5d3bfba3a
jithin779 <jithinjoji51@gmail.com>
2025-09-09 15:23:02 +0530

    Update 1.c

--- javascript basics/1.c
+++ javascript basics/1.c
@@ -1,78 +1,184 @@
-#include<stdio.h>
-int front=-1,rear=-1,choice,size,item,q[10],i;
-void Enqueue();
-void Dequeue();
-void Display();
-int main()
+#include<math.h>
+#include<ctype.h>
+#include<stdlib.h>
+char s[50],E[100],item;
+int n,i,top=-1;
+	
+	void infix();
+	void evalPostfix();
+	void push(char item);
+	char pop();
+	int Priority(char sym);
+		
+		int main()
+		{
+		int ch;
+		while(1){
+		printf("\n----MENU----\n");
+		printf("1.infix to postfix conversion\n");
+		printf("2.evaluate postfix expression\n");
+		printf("3.Exit\n");
+		printf("enter the choice:");
+			scanf("%d",&ch);
+			switch(ch){
+			case 1:
+			top=-1;
+			printf("enter size of expression");
+			scanf("%d",&n);
+				printf("enter the infix expression:");
+				scanf("%s",E);
+				printf("postfix expression:");
+				infix();
+				printf("\n");
+				break;
+				case 2:
+				evalPostfix();
+				break;
+				case 3:
+				exit(0);
+				default:
+				printf("invalid choice\n");
+				}
+				}
+				return 0;	
+					}
+					void push(char item)
+					{
+					if(top==n-1)
+					printf("stack full\n");
+					else
+					s[++top]=item;
+					}
+					char pop()
+					{
+					if (top==-1){
+					printf("stack empty\n");
+					return '\0';
+					}else{
+					return s[top--];
+					}
+					}
+					int Priority(char sym)
+					{
+					switch(sym){
+					case'^':return 3;
+					case'*':
+					case'/':return 2;
+					case'+':
+					case'-':return 0;
+					}
+					}
+						void infix()
+						{
+						push('(');
+						strcat(E,")");
+						for(i=0;E[i]!='\0';i++){
+						item=E[i];
+						if(item=='('){
+						push(item);
+						}else if (isalnum(item))
+		{
+		printf("%c",item);
+		}
+		else if (item=='^'|| item=='*'||item=='/'||item=='+'||item=='-'){
+		while(top!=-1&&((Priority(s[top])>Priority(item))||(Priority(s[top])==Priority(item)&&item!='^'))&&
+		s[top]!='('){
+		printf("%c",pop());
+		}
+		push(item);
+		} else if (item==')'){
+		while(top!=-1&&s[top]!='('){
+		printf("%c",pop());
+		}
+		pop();
+		}
+		else
+		{
+		printf("invalid expression\n");
+		break;
+		}
+		}
+		}
+		void evalPostfix()
+		{
+		char expr[100];
+		int stack[50],evalTop=-1;
+		char ch;
+		int val,a,b,result;
+		printf("enter postfix epression(a+b*c):");
+		scanf("%s",expr);
+		for(i=0; expr[i]!='\0';i++){
+		ch=expr[i];
+		if(isalpha(ch)){
+		printf("enter the value for %c:",ch);
+scanf("%d",&val);
+stack[++evalTop]=val;
+}
+else if (isdigit(ch))
 {
-
-printf("enter the size of the queue");
-scanf("%d",&size);
-do{
-printf("enter the your choice:\n");
-printf("\n1.ENQUEUE\t2.DEQUEUE\t 3.DISPLAY\t 4.EXIT\n");
-scanf("%d",&choice);
-switch(choice)
+stack[++evalTop]=ch- '0';
+}
+else
 {
-case 1:Enqueue();
+if (evalTop<1){
+printf("Error:not enough operands\n");
+return;
+}
+b=stack[evalTop--];
+a=stack[evalTop--];
+switch (ch) {
+case'+':result=a+b;
 break;
-case 2:Dequeue();
+case'-':result=a-b;
 break;
-case 3:Display();
+case'*':result=a*b;
 break;
-case 4:printf("Exit");
-return 0;
-default:printf("Invalid Choice\n");
+case'/':
+if(b==0){
+printf("error:division by zero\n");
+return;
 }
-}while(choice!=4);
+result=a/b;
+break;
+//case'^':result=pow(a,b);
+break;
+default:
+printf("invalid operator %c\n",ch);
+return;
 }
+stack[++evalTop]=result;
+}
+}
+if(evalTop!=0){
+printf("error:invalid postfix expression\n");
+return;
+}
+printf("result=%d\n",stack[evalTop]);
+}				
 
-void Enqueue()
-{
-if(rear==size-1){
-printf("queue is full\n");
-}
-else if(front==-1&&rear==-1){
-front=rear=0;
-printf("ENTER THE ELEMENT : ");
-scanf("%d",&item);
-q[rear]=item;
-}
-else{
-printf("ENTER THE ELEMENT : ");
-scanf("%d",&item);
-rear=rear+1;
-q[rear]=item;
-}printf("\n");
-}
+output
+----MENU----
+1.infix to postfix conversion
+2.evaluate postfix expression
+3.Exit
+enter the choice:1
+enter size of expression7
+enter the infix expression:(7*3)+2
+postfix expression:73*2+
 
-void Dequeue()
-{
-if(front==-1&&rear==-1){
-printf("queue is empty");
-}
-else if(front=rear){
-printf("deleted item is :%d\t",q[front]);
-}
-else{
-printf("deleted item is :%d\t",q[front]);
-front=front+1;
-}printf("\n\n");
-}
+----MENU----
+1.infix to postfix conversion
+2.evaluate postfix expression
+3.Exit
+enter the choice:2
+enter postfix epression(a+b*c):73*2+
+result=23
 
-void Display()
-{
-if(front==-1&&rear==-1){
-printf("queue is empty");
-}
-else{
-for(i=front;i<=rear;i++){
-printf("%d\t",q[i]);
-}
-}printf("\n\n");
-}
-
-
+----MENU----
+1.infix to postfix conversion
+2.evaluate postfix expression
+3.Exit
+enter the choice:3
 
 
 
